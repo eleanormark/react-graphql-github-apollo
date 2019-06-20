a5ad8428b490a0dc75f1c49a19eda63b926e1c6d
Eleanor Mark <esm@Eleanors-MBP.attlocal.net>
2019-06-19 20:01:36 -0700

    Add local state management with Apollo Client.

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
-
-import RepositoryList from "../Repository";
+import RepositoryList, { REPOSITORY_FRAGMENT } from '../Repository';
 import Loading from "../Loading";
 import ErrorMessage from "../Error";
 
@@ -18,60 +17,19 @@ const GET_CURRENT_USER = gql`
 const GET_REPOSITORIES_OF_CURRENT_USER = gql`
   {
     viewer {
-      repositories(first: 10, orderBy: { direction: DESC, field: STARGAZERS }) {
+      repositories(
+        first: 10
+        orderBy: { direction: DESC, field: STARGAZERS }
+      ) {
         edges {
           node {
-            id
-            name
-            url
-            descriptionHTML
-            primaryLanguage {
-              name
-            }
-            owner {
-              login
-              url
-            }
-            stargazers {
-              totalCount
-            }
-            viewerHasStarred
-            watchers {
-              totalCount
-            }
-            viewerSubscription
+            ...repository
           }
         }
       }
     }
   }
-`;
-
-const GET_CURRENT_USER_STARRED_REPO = gql`
-  {
-    viewer {
-      login
-      starredRepositories(last: 10) {
-        edges {
-          node {
-            name
-            url
-            stargazers {
-              totalCount
-            }
-            descriptionHTML
-            primaryLanguage {
-              name
-            }
-            owner {
-              login
-              url
-            }
-          }
-        }
-      }
-    }
-  }
+  ${REPOSITORY_FRAGMENT}
 `;
 
 const Profile = () => (
--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -1,30 +1,64 @@
 import React from "react";
-import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
+import REPOSITORY_FRAGMENT from '../fragments';
 import Link from "../../Link";
 import Button from "../../Button";
 
-const STAR_REPOSITORY = gql`
-  mutation($id: ID!) {
-    addStar(input: { starrableId: $id }) {
-      starrable {
-        id
-        viewerHasStarred
-      }
-    }
-  }
-`;
+import {
+  STAR_REPOSITORY,
+  UNSTAR_REPOSITORY,
+} from '../mutations';
 
-const UN_STAR_REPOSITORY = gql`
-  mutation($id: ID!) {
-    removeStar(input: { starrableId: $id }) {
-      starrable {
-        id
-        viewerHasStarred
-      }
-    }
-  }
-`;
+const updateAddStar = (
+  client,
+  {
+    data: {
+      addStar: {
+        starrable: { id, viewerHasStarred },
+      },
+    },
+  },
+) =>
+  client.writeFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+    data: getUpdatedStarData(client, id, viewerHasStarred),
+  });
+
+const updateRemoveStar = (
+  client,
+  {
+    data: {
+      removeStar: {
+        starrable: { id, viewerHasStarred },
+      },
+    },
+  },
+) => {
+  client.writeFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+    data: getUpdatedStarData(client, id, viewerHasStarred),
+  });
+};
+
+const getUpdatedStarData = (client, id, viewerHasStarred) => {
+  const repository = client.readFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+  });
+
+  let { totalCount } = repository.stargazers;
+  totalCount = viewerHasStarred ? totalCount + 1 : totalCount - 1;
+
+  return {
+    ...repository,
+    stargazers: {
+      ...repository.stargazers,
+      totalCount,
+    },
+  };
+};
 
 const RepositoryItem = ({
   id,
@@ -41,14 +75,10 @@ const RepositoryItem = ({
       <div className="RepositoryItem-title-url">
         <Link href={url}>{name}</Link>
       </div>
-
-      <div className="RepositoryItem-title-action">
-        {stargazers.totalCount} Stars
-      </div>
     </div>
     <div>
       {!viewerHasStarred ? (
-        <Mutation mutation={STAR_REPOSITORY} variables={{ id }}>
+        <Mutation mutation={STAR_REPOSITORY} variables={{ id }} update={updateAddStar}>
           {(addStar, { data, loading, error }) => (
             <Button className={"RepositoryItem-title-action"} onClick={addStar}>
               {stargazers.totalCount} Star
@@ -58,7 +88,7 @@ const RepositoryItem = ({
       ) : (
         <span>
           {
-            <Mutation mutation={UN_STAR_REPOSITORY} variables={{ id }}>
+            <Mutation mutation={UNSTAR_REPOSITORY} variables={{ id }} update={updateRemoveStar}>
               {(removeStar, { data, loading, error }) => (
                 <Button
                   className={"RepositoryItem-title-action"}
