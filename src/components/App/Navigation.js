8b8580ccef3cb3995a41f814098021b9b061347f
eleanor <eleanorsmark@yahoo.com>
2019-07-01 11:12:14 -0700

    Updated Navigation.js for snapshot tests.

--- src/components/App/Navigation.js
+++ src/components/App/Navigation.js
@@ -69,7 +69,7 @@ const NavSearch = styled.div`
   }
 `;
 
-const Navigation = ({
+export const Navigation = ({
   location: { pathname },
   queryString,
   setQuerySearch
@@ -89,7 +89,7 @@ const Navigation = ({
   </Nav>
 );
 
-const QuerySearch = props => {
+export const QuerySearch = props => {
   const [term, setTerm] = useState(props.queryString);
   return (
     <NavSearch>
