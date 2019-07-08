2d3e44fb19f84e030cbff2329da6bb4f2f5396a2
eleanormark <eleanormark@gmail.com>
2019-07-08 15:29:26 -0700

    Extended fixture.

--- src/tests/fixture/repos.js
+++ src/tests/fixture/repos.js
@@ -1,10 +1,21 @@
 export default [{
-    id: '1',
-    name: 'qwerty',
-    url: 'qwerty',
-    descriptionHTML: 'qwerty',
-    owner: {url: 'qwerty'},
-    stargazers: {totalCount: 1},
-    viewerHasStarred: true,
-    primaryLanguage: {name: 'java'}
-  }];
+  id: '1',
+  name: 'eleanor',
+  url: 'eleanor@example.com',
+  descriptionHTML: 'test description',
+  owner: {url: 'eleanor@example.com'},
+  stargazers: {totalCount: 1},
+  viewerHasStarred: true,
+  primaryLanguage: {name: 'javascript'}
+},
+{
+  id: '2',
+  name: 'ellie',
+  url: 'ellie@example.com',
+  descriptionHTML: 'test description for ellie',
+  owner: {url: 'ellie@example.com'},
+  stargazers: {totalCount: 2},
+  viewerHasStarred: true,
+  primaryLanguage: {name: 'ruby'}
+}
+];
